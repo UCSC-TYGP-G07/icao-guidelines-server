2cd607a734f3328584ad3d0bcf857b66076c0718
Ramindu Walgama <raminduw200@gmail.com>
2023-09-30 12:19:11 +0530

    feat: make function callable and update thresholds according to the dataset
    Replace direct run with a function and update the thresholds by checking the given dataset

--- detectors/geometric_tests/geometric_tests.py
+++ detectors/geometric_tests/geometric_tests.py
@@ -6,6 +6,13 @@ import os
 
 dirname = os.path.dirname(__file__)
 
+MY_THRESHOLD = (0.3, 0.5)
+MX_THRESHOLD = (0.45, 0.55)
+# CC_THRESHOLD = (0.5, 0.75)
+CC_THRESHOLD = (0.45, 0.7)
+# DD_THRESHOLD = (0.6, 0.9)
+DD_THRESHOLD = (0.35, 0.65)
+
 
 def get_face_landmarks(image_path):
     model_path = os.path.join(dirname, "face_landmarker.task")
@@ -27,33 +34,33 @@ def get_face_landmarks(image_path):
 # Function to check the vertical position
 def geometric_test_vertical_position(face_landmarks):
     My = (face_landmarks[168].y + face_landmarks[6].y) / 2
-    return My >= 0.3 and My <= 0.5
+    return MY_THRESHOLD[0] <= My <= MY_THRESHOLD[1]
 
 
 # Function to check the horizontal position
 def geometric_test_horizontal_position(face_landmarks):
     Mx = (face_landmarks[234].x + face_landmarks[454].x) / 2
-    return Mx >= 0.45 and Mx <= 0.55
+    return MX_THRESHOLD[0] <= Mx <= MX_THRESHOLD[1]
 
 
 # Function to check the head image width ratio
 def geometric_test_head_image_width_ratio(face_landmarks):
     dy = face_landmarks[454].y - face_landmarks[234].y
     dx = face_landmarks[454].x - face_landmarks[234].x
-    CC = ((dy**2) + (dx**2)) ** 0.5
-    return CC >= 0.5 and CC <= 0.75
+    CC = ((dy ** 2) + (dx ** 2)) ** 0.5
+    return CC_THRESHOLD[0] <= CC <= CC_THRESHOLD[1]
 
 
 # Function to check the head image height ratio
 def geometric_test_head_image_height_ratio(face_landmarks):
     dy = face_landmarks[152].y - face_landmarks[10].y
     dx = face_landmarks[152].x - face_landmarks[10].x
-    DD = ((dy**2) + (dx**2)) ** 0.5
-    return DD >= 0.6 and DD <= 0.9
+    DD = ((dy ** 2) + (dx ** 2)) ** 0.5
+    return DD_THRESHOLD[0] <= DD <= DD_THRESHOLD[1]
 
 
-if __name__ == "__main__":
-    image_path = os.path.join(dirname, "Profile-pic.jpeg")
+def valid_geometric(image_path):
+    # image_path = os.path.join(dirname, "Profile-pic.jpeg")
     face_landmarks = get_face_landmarks(image_path)
     # Geometric test - Vertical position
     vp_check = geometric_test_vertical_position(face_landmarks)
@@ -64,7 +71,12 @@ if __name__ == "__main__":
     # Geometric test - Head image height ratio
     hihr_check = geometric_test_head_image_height_ratio(face_landmarks)
 
-    if vp_check and hp_check and hiwr_check and hihr_check:
-        print("Geometric tests passed!")
-    else:
-        print("Geometric tests failed!")
+    data = {
+        "valid": vp_check and hp_check and hiwr_check and hihr_check,
+        "vertical_position": vp_check,
+        "horizontal_position": hp_check,
+        "head_image_width_ratio": hiwr_check,
+        "head_image_height_ratio": hihr_check
+    }
+
+    return data
